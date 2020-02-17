e4dc37791f80fe4c0cb26476603331849a373723
Valentin Nikolov <valentin.nikolov@falcon.io>
2020-02-17 19:05:56 +0200

    use constructor with autowired

--- weather-getter/src/main/java/homework/rest/WeatherController.java
+++ weather-getter/src/main/java/homework/rest/WeatherController.java
@@ -3,6 +3,7 @@ package homework.rest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import homework.impl.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WeatherController {
 
-    WeatherService weatherService = new WeatherService();
+    WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
 
     @GetMapping("weather/current")
     public JsonNode getCurrentWeatherByCity(@RequestParam(value = "location", defaultValue = "Sofia") String location) throws JsonProcessingException {
