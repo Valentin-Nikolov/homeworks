bec66b4ddcf58450222e75fe7c03f033a50b0993
Valentin Nikolov <valentin.nikolov@falcon.io>
2020-02-24 16:52:42 +0200

    renamed application class file

--- spotify-jpa/src/main/java/spotify/jpa/SpotifyJpaApplication.java
+++ spotify-jpa/src/main/java/spotify/jpa/SpotifyJpaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataRestApplication {
+public class SpotifyJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataRestApplication.class, args);
+		SpringApplication.run(SpotifyJpaApplication.class, args);
 	}
 }
